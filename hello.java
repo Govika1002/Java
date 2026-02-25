92b511f4da78dd66657c6bede263e48b81b58fa3
Govika M <govikamarimuthu10@gmail.com>
2026-02-25 18:28:49 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class hello{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+
+        //DATATYPES AND VARIABLES
+
+        int container = 67;
+        String John = "J";
+        String container2 = John;
+        System.out.println(container);
+        System.out.println(container2);
+        int a = 20;
+        int b = 30;
+        int c = 40;
+        int d = a+b+c;
+        System.out.print(d);
+
+        
+    }
+}
---
+++ integer.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class integer{
+      
+      public static void main(String args[]){
+ //INTEGER
+
+      Scanner scan =new Scanner(System.in);
+      int a = scan.nextInt();
+      int b = scan.nextInt();
+      System.out.print(a+b);
+
+
+      }
+      
+}
